890682a00ce8d1d80fd2473d37d2d19ba14a673b
Finn Beilke <beilke693@gmail.com>
2024-02-24 17:58:15 -0600

    Create GroupImages table and model, add options to memberships as well

--- backend/db/migrations/20240224235131-create-group-image.js
+++ backend/db/migrations/20240224235131-create-group-image.js
@@ -5,22 +5,15 @@ if (process.env.NODE_ENV === 'production') {
  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Memberships', {
+    await queryInterface.createTable('GroupImages', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {model: 'Users'},
-        onDelete: 'CASCADE'
       },
       groupId: {
         type: Sequelize.INTEGER,
@@ -28,23 +21,26 @@ module.exports = {
         references: {model: 'Groups'},
         onDelete: 'CASCADE'
       },
-      status: {
-        type: Sequelize.ENUM(['active', 'pending-approval'])
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      preview: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        type: Sequelize.DATE
       }
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Memberships'
+    options.tableName = 'GroupImages'
     await queryInterface.dropTable(options);
   }
 };
