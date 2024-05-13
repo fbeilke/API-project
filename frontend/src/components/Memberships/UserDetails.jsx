b40f7aa6a98ce16c8174355debfdfef204776bb0
Finn Beilke <beilke693@gmail.com>
2024-05-13 18:14:17 -0500

    npm run build

--- frontend/src/components/Memberships/UserDetails.jsx
+++ frontend/src/components/Memberships/UserDetails.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux"
+import { useNavigate } from 'react-router-dom';
 
 export default function UserDetails() {
+    const navigate = useNavigate();
     const { user } = useSelector(state => state.session)
 
 
